44e50872d5761eee003a3b1eeb5271a8c36e4e21
Agacy Junior <juniormartinssom@gmail.com>
2024-09-29 17:46:45 -0300

    GetByText

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { SelectList } from "@components/SelectList";
+
+describe("Component: SelectList", () => {
+  it("Should be return city details selected.", () => {
+    const data = [
+      {
+        id: "1",
+        name: "Campinas",
+        latitude: 123,
+        longitude: 456,
+      },
+      {
+        id: "2",
+        name: "São Paulo",
+        latitude: 789,
+        longitude: 102,
+      },
+    ];
+
+    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+
+    const selectedCity = screen.getByText("São Paulo");
+  });
+});
