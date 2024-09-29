e517fcd3e154212babe7e8411ddc485749365c35
Agacy Junior <juniormartinssom@gmail.com>
2024-09-29 17:36:39 -0300

    toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,8 +4,16 @@ import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
   it("Should be render without activity indicator if isLoading prop is undefined", () => {
+    render(<Input />);
+
+    const activityIndicator = screen.queryByTestId("activity-indicator");
+    expect(activityIndicator).toBeNull();
+  });
+
+  it("Should be render with activity indicator if isLoading prop is true.", () => {
     render(<Input isLoading />);
 
     const activityIndicator = screen.getByTestId("activity-indicator");
+    expect(activityIndicator).toBeTruthy();
   });
 });
