a6655681bdcfff505da796d38c532035bd145bb4
Agacy Junior <juniormartinssom@gmail.com>
2024-09-29 17:29:25 -0300

    testID

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,10 +1,11 @@
-import { render } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 
 import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
-  it("Should be render without activity indicator", () => {
-    const { debug } = render(<Input />);
-    debug();
+  it("Should be render without activity indicator if isLoading prop is undefined", () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator");
   });
 });
