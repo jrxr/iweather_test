fcb5d829f90dbc5c8ff6f309fa9e7432be1e5b3b
Agacy Junior <juniormartinssom@gmail.com>
2024-09-30 19:44:19 -0300

    waitFor and act

--- src/contexts/cityContext.test.tsx
+++ src/contexts/cityContext.test.tsx
@@ -1,8 +1,11 @@
-import { renderHook } from "@testing-library/react-native";
+import { act, renderHook, waitFor } from "@testing-library/react-native";
 import { useCity } from "@hooks/useCity";
+import { CityProvider } from "@contexts/CityContext";
 
 describe("Context: CityContext", () => {
-  it("Should be change selected city", () => {
-    const { result } = renderHook(() => useCity());
+  it("Should be change selected city", async () => {
+    const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
+
+    await waitFor(() => act(() => console.log(result.current)));
   });
 });
