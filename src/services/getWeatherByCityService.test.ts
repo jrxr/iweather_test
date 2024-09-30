6940fe9cd5c0347c5566b299391221b6f0047e15
Agacy Junior <juniormartinssom@gmail.com>
2024-09-30 18:48:10 -0300

    Testing returns from the API

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,16 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/mockWeatherAPIResponse";
+import { api } from "./api";
+import { getWeatherByCityService } from "./getWeatherByCityService";
+
+describe("API: getWeatherByCityService", () => {
+	it("Should be returnweather api data formatted", async () => {
+		jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
+		const response = await getWeatherByCityService({
+			latitude: 123,
+			longitude: 456,
+		});
+
+		expect(response).toHaveProperty("today");
+	});
+});
