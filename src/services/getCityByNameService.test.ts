ce4d21410b8ca8fbe35864b64ee1a05f98d322f2
Agacy Junior <juniormartinssom@gmail.com>
2024-09-30 18:39:57 -0300

    Mock folder

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,23 +1,12 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 
 describe("API: getCityByNameService", () => {
 	it("Should return city details", async () => {
-		const data = {
-			id: "1",
-			name: "São Paulo",
-			sys: {
-				country: "BR",
-			},
-			coord: {
-				lat: 123,
-				lon: 456,
-			},
-		};
+		jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
+		const response = await getCityByNameService("São Paulo");
 
-		jest.spyOn(api, "get").mockResolvedValue({ data });
-    const response = await getCityByNameService("São Paulo");
-
-    expect(response.length).toBeGreaterThan(0);
+		expect(response.length).toBeGreaterThan(0);
 	});
 });
