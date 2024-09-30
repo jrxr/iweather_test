7584eb208120ba6dc467d3a28539bfd2983433ec
Agacy Junior <juniormartinssom@gmail.com>
2024-09-30 19:06:41 -0300

    saveStorageCity test

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,28 @@
-import { getStorageCity } from "@libs/asyncStorage/cityStorage";
+import {
+	getStorageCity,
+	saveStorageCity,
+} from "@libs/asyncStorage/cityStorage";
+import type { CityProps } from "@services/getCityByNameService";
 
 describe("Storage: CityStorage", () => {
 	it("Should be return null when don't have a city storaged", async () => {
 		const response = await getStorageCity();
 
 		expect(response).toBeNull();
+	});
+
+	it("Should be return city storage.", async () => {
+		const newCity: CityProps = {
+			id: "1",
+			name: "São Paulo",
+			latitude: 123,
+			longitude: 456,
+		};
+
+		await saveStorageCity(newCity);
+
+		const response = await getStorageCity();
+
+		expect(response).toEqual(newCity);
 	});
 });
