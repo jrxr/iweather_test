3df1a8457172ac1c53751e2649b712e0946dccf4
Agacy Junior <juniormartinssom@gmail.com>
2024-10-01 20:41:43 -0300

    Testing the Dashboard

---
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -0,0 +1,26 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
+import { render, waitFor, screen } from "@__tests__/utils/customRender";
+import { api } from "@services/api";
+import { Dashboard } from "@screens/Dashboard";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+
+describe("Screen: Dashboard", () => {
+  it("Should be show city weather.", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const city = {
+      id: "1",
+      name: "Rio do Sul, BR",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    render(<Dashboard />);
+
+    const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
+
+    expect(cityName).toBeTruthy();
+  });
+});
