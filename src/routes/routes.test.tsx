0e1720cf0516d614367c3cdc0c0b64d8abdccefb
Agacy Junior <juniormartinssom@gmail.com>
2024-10-01 08:49:26 -0300

    CustomRender

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react-native";
+import { render, screen, waitFor } from "@__tests__/utils/customRender";
 import { Routes } from ".";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 
 describe("Routes", () => {
   it("Should be render Search screen when not city selected.", async () => {
@@ -8,5 +9,18 @@ describe("Routes", () => {
     const title = await waitFor(() => screen.findByText(/^escolha um local/i));
 
     expect(title).toBeTruthy();
+  });
+
+  it("Should be render Dashboard screen when has city selected.", async () => {
+    const city = {
+      id: "1",
+      name: "São Paulo",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+    
+    render(<Routes />);
   });
 });
